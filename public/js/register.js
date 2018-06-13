3d7cdc425d820ce9973f45566d23460a0e22c876
Jerg Bengel <jerg.bengel@hotmail.de>
2018-06-13 17:50:26 +0200

    register.js ausgelagert

---
+++ public/js/register.js
@@ -0,0 +1,22 @@
+﻿//Tom Maier, 751605; Jerg Bengel, 752685
+
+function init() {
+
+    $('#picture').before('<input type="button" id="button-file" class=" form-control btn btn-md btn-primary btn-block" value="Add File" onchange="checkIfMsgEx()"/> ');
+    $('#picture').hide();
+    $('body').on('click', '#button-file', function () {
+        $('#picture').trigger('click');
+    });
+}
+
+function nospaces(t) {
+
+    if (t.value.match(/\s/g)) {
+
+        alert('Sorry, you are not allowed to enter any spaces');
+
+        t.value = t.value.replace(/\s/g, '');
+
+    }
+
+}
